2768f75c6900d21e215a44db36be4072fdc10e03
paul-ilies <pauldaniel.ilies@gmail.com>
2020-11-16 10:34:22 +0200

    project finished

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#253f56').all(10));
 
   const handlerSubmit = (e) => {
     e.preventDefault();
-    console.log('Hello');
+
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true)
+      console.log(error)
+
+    }
   }
 
 
@@ -19,13 +27,17 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handlerSubmit}>
-          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' />
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`} />
           <button className="btn" type='submit'>Submit</button>
         </form>
       </section>
 
       <section className="colors">
-        <h4>color list</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor key={index} {...color} index={index} />
+          )
+        })}
       </section>
 
     </React.Fragment>
